10f25b1ee1666fdb67eb2d884e18a2a5dc4f9c87
LianthSoftware <LianthSoftware@users.noreply.github.com>
2025-10-01 13:14:48 +0000

    deploy: dbfd8911f193f809a84a037bbf1022cd538d6818

---
+++ js/culture.js
@@ -0,0 +1,5 @@
+﻿window.culture = {
+    get: () => localStorage.getItem("ui-culture"),
+    set: (lang) => localStorage.setItem("ui-culture", lang),
+    browser: () => (navigator.language || "en").split("-")[0] // "en-US" -> "en"
+};
